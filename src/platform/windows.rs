437fe16fe1c0e70abb80e756740f53bd71d5fa85
Lirons01 <irons2@purdue.edu>
2022-11-03 18:14:55 -0400

    Added audio output + needed dependencies

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -130,6 +130,29 @@ pub fn start_event_loop(mut tree: Box<dyn ComponentTraits>) {
 
         Ok(())
     }
+
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::time::Duration;
+    use rodio::{Decoder, OutputStream, Sink};
+    use rodio::source::{SineWave, Source};
+
+    fn play_sound() {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+
+        // Add a dummy source of the sake of the example.
+        let source_A = SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.5)).amplify(0.20);
+        let source_Csharp = SineWave::new(554.37).take_duration(Duration::from_secs_f32(0.5)).amplify(0.20);
+        let source_E = SineWave::new(659.25).take_duration(Duration::from_secs_f32(0.5)).amplify(0.20);
+        sink.append(source_A);
+        sink.append(source_Csharp);
+        sink.append(source_E);
+
+        // The sound plays in a separate thread. This call will block the current thread until the sink
+        // has finished playing all its queued sounds.
+        sink.sleep_until_end();
+    }
     
     #[allow(deprecated)]
     el.run(move |event, _, control_flow| {
@@ -172,7 +195,8 @@ pub fn start_event_loop(mut tree: Box<dyn ComponentTraits>) {
                 } => {
                     if state == ElementState::Pressed {
                         handle_click(last_postition, state, button);
-                        send_request();
+                        //send_request();
+                        play_sound();
                     }
                 }
                 _ => (),
